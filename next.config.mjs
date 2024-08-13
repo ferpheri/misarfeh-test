0fe129bff836356be666e9bd45bce45ea35c3fb5
ferpheri <dehghanian77@gmail.com>
2024-08-13 20:38:38 +0330

    pwa commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+    dest: "public"
+})
 const nextConfig = {};
 
-export default nextConfig;
+module.exports = withPWA(nextConfig)
